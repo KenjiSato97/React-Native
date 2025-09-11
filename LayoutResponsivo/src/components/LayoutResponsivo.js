358e1f7a01f60c4b8ff73e7eebb1ecb44d84e8f6
Kenji Sato <knjsa@example.com>
2025-09-11 20:32:49 -0300

    Add: ajustes nas views

--- LayoutResponsivo/src/components/LayoutResponsivo.js
+++ LayoutResponsivo/src/components/LayoutResponsivo.js
@@ -3,27 +3,29 @@ import React from 'react';
 import { Image } from 'react-native';
 
 const LayoutResponsivo = () => {
+  const handleGithubPress = () => {
+    alert('Abrindo Github');
+  };
+  const handleLinkedinPress = () => {
+    alert('Abrindo Linkedin');
+  };
+
+
   return (
     <View style={styles.container}>
         <View style={styles.header}> 
         </View>
         <View style={styles.main}>
           <View style={styles.card}>
-            <View style={styles.profileContainer}>
-              <View/>
               <Image
                 source={require('../images/profile-picture.png')}
-                style={styles.avatar}
-              />
-              <View style={styles.info}>
+                style={styles.avatar}/>
                 <Text style={styles.name}>Kenji Sato</Text>
                 <Text style={styles.description}>Engenheiro/Analista de dados, entusiasta da cultura Data driven</Text>
                 <View style={styles.buttonContainer}>
-                  <Text style={styles.button2} onPress={() => { /* abrir github */ }}>Github</Text>
-                  <Text style={styles.button} onPress={() => { /* abrir linkedin */ }}>Linkedin</Text>
+                  <Text style={styles.button2} onPress={handleGithubPress}>Github</Text>
+                  <Text style={styles.button} onPress={handleLinkedinPress}>Linkedin</Text>
                 </View>
-              </View>
-            </View>
           </View>
         </View>
         <View style={styles.footer}> 
@@ -38,9 +40,7 @@ const styles = StyleSheet.create({
   main: { flex: 3, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'},
   footer: { flex: 0.3, backgroundColor: 'black'},
   card: { width: '90%', height: '50%', backgroundColor: '#f0f0f0', borderRadius: 10, padding: 20, elevation: 5, alignItems: 'center', justifyContent: 'center' },
-  profileContainer: { flexDirection: 'column', alignItems: 'center' },
   avatar: { width: 100, height: 100, borderRadius: 50, borderWidth: 3, borderColor: 'black' },
-  info: { flex: 1, alignItems: 'center' },
   name: { fontSize: 24, fontWeight: 'bold' },
   description: { fontSize: 16, textAlign: 'center', marginTop: 5 },
   buttonContainer: { flexDirection: 'row', marginTop: 10, justifyContent: 'space-between', width: '70%' },
